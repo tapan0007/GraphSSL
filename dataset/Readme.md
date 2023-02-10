c12df76c8c81b2f723a2942c4be2e1fb1a5be15d
kiddycharles <juy022@ucsd.edu>
2023-02-09 21:31:55 -0800

    GCN implementation using DGL and data loader function for ellipticGraph.

---
+++ dataset/Readme.md
@@ -0,0 +1,39 @@
+## Readme
+
+*This file describes the topological properties summarized from elliptic dataset.*  
+
+#### Components  
+- ellicpticGraph_pimg
+  - access: dgl.load_graphs(path)[0][0]
+  - homogeneous, undirected graph contains nodal level persistent images
+  - DGL graph format (torch backend)
+  - ndata['features'] $X\in R^{203769\times 166}$ nodel level features
+  - ndata['pimgs0'] $H^{(0)}\in R^{203769\times 64}$ persistent images at homology dimension H0
+  - ndata['pimgs1'] $H^{(1)}\in R^{203769\times 64}$ persistent images at homology dimension H1  
+- labels 
+  - access: dgl.load_graphs(path)[1]
+  - key 'known_ids' and 'known_labs', the corresponding index and labels of nodes  
+  - label 0: benign, label 1: anomaly 
+  - labels of around three quarters of nodes are unknown, please ignore these for downstream task
+- diagrams  
+  - json dict file, loaded with json.loads() 
+  - key 'H0': list of persistent diagrams at homology dimenstion H0 
+  - key 'H1': list of persistent diagrams at homology dimenstion H1
+  - each diagram summarizes a node neighborhood structrue, so the length of the list ('H0' or 'H1') is 203769
+  - each diagram is an $N\times 2$ array, but please be careful about the variable length, *i.e.* $N$ is not a fixed constant
+  - there may exist some emtpy diagrams 
+
+#### Others
+1. Persistent images (pimgs0/pimgs1) and persistent diagrams (diagrams['H0']/diagrams['H1']) 
+   Persistent diagram is computed according to 3-hop neighborhood structure of the target node. It is a set of 2D points. 
+   Persistent image is the vectorized representation of a diagram, and can be viewed as a fixed length vector. 
+   Both diagram and image are the instances of topological structure summaries, so please be sure to use only one of them for supervision signals.  
+   There are two dimensions of persistent summaries, and the more important one is dimension 0, but dimension 1 also provides additional topological information. For simplicity, we could start from using dimension 0 and then incorporate with dimension 1. 
+2. Pariwise distance between diagrams 
+   Since diagram is a set with variable length and it is difficult to directly predict a set, we can sample a pair of nodes randomly and then predict their pairwise distance between the diagrams.  
+   Here we could use bottleneck distance between two diagrams, $d: Dgm \times Dgm \to R$, which takes input of two diagrams and returns a scalar distance. 
+   One efficient approximation to do such calculation: [gudhi.hera.bottleneck_distance](https://gudhi.inria.fr/python/latest/bottleneck_distance_user.html).  
+   But feel free to explore any distance map defined for sets, *e.g.* chamfer distance, wasserstein distance. 
+3. Predicting persistent images
+   I also provide another topology representation, persistent images. 
+   For images, it is quite simple to develop SSL principles, since it is a fixed length vector.
---
+++ gcn.py
@@ -0,0 +1,97 @@
+import dgl
+import dgl.function as fn
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+import sklearn.model_selection as sk
+import numpy as np
+
+gcn_msg = fn.copy_u(u='h', out='m')
+gcn_reduce = fn.sum(msg='m', out='h')
+
+
+class GCNLayer(nn.Module):
+    def __init__(self, in_feats, out_feats):
+        super(GCNLayer, self).__init__()
+        self.linear = nn.Linear(in_feats, out_feats)
+
+    def forward(self, g, feature):
+        # Creating a local scope so that all the stored ndata and edata
+        # (such as the `'h'` ndata below) are automatically popped out
+        # when the scope exits.
+        with g.local_scope():
+            g.ndata['h'] = feature
+            g.update_all(gcn_msg, gcn_reduce)
+            h = g.ndata['h']
+            return self.linear(h)
+
+
+class Net(nn.Module):
+    def __init__(self, in_dim, hidden_dim, out_dim):
+        super(Net, self).__init__()
+        self.layer1 = GCNLayer(in_dim, hidden_dim)
+        self.layer2 = GCNLayer(hidden_dim, out_dim)
+
+    def forward(self, g, features):
+        x = F.relu(self.layer1(g, features))
+        x = self.layer2(g, x)
+        return x
+
+
+def load_elliptic_data(path):
+    dataset = dgl.load_graphs(path)
+    g = dataset[0][0]
+    feats = g.ndata['features']
+    # Below can be removed if test metrics do not perform well
+    graph_structure = dgl.load_graphs('dataset/ellipticGraph_pimg')
+    pimg0 = graph_structure[0][0].ndata['pimgs0']
+    pimg1 = graph_structure[0][0].ndata['pimgs1']
+    feats = th.cat((feats, pimg0, pimg1), 1)
+    # Till here
+    n_nodes, feat_dim = feats.shape[0], feats.shape[1]
+    known_ids = np.array(dataset[1]['ids'])
+    node_labels = dataset[1]['labels']
+    train_nid, test_nid, train_l, test_l = sk.train_test_split(known_ids, node_labels, test_size=0.6, random_state=42)
+    return g, feats, n_nodes, feat_dim, train_nid, test_nid, train_l, test_l
+
+
+def evaluate(model, g, features, labels, mask):
+    model.eval()
+    with th.no_grad():
+        logits = model(g, features)
+        logits = logits[mask]
+        _, indices = th.max(logits, dim=1)
+        correct = th.sum(indices == labels)
+        return correct.item() * 1.0 / len(labels)
+
+
+if __name__ == '__main__':
+
+    g, features, num_nodes, feature_dim, train_ids, test_ids, train_labels, test_labels = load_elliptic_data(
+        'dataset/ellipticGraph')
+    net = Net(feature_dim, 100, 2)
+
+    g.add_edges(g.nodes(), g.nodes())
+
+    optimizer = th.optim.Adam(net.parameters(), lr=1e-2)
+    dur = []
+    for epoch in range(100):
+        if epoch >= 3:
+            t0 = time.time()
+
+        net.train()
+        logits = net(g, features)
+        logp = F.log_softmax(logits, 1)
+        loss = F.nll_loss(logp[train_ids], train_labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if epoch >= 3:
+            dur.append(time.time() - t0)
+
+        acc = evaluate(net, g, features, test_labels, test_ids)
+        print("Epoch {:05d} | Loss {:.4f} | Test Acc {:.4f} | Time(s) {:.4f}".format(
+            epoch, loss.item(), acc, np.mean(dur)))

a519ebee05dde60f77bd27077103919b2144595d
Nigel Doering <nfdoerin@ucsd.edu>
2023-03-14 12:01:59 -0700

    Refactor performance.py

--- performance.py
+++ performance.py
@@ -39,38 +39,51 @@ def performance_metrics_ssl(classifier, model, g, features, labels, mask):
     g_mean = geometric_mean_score(labels, preds)
     return f1, g_mean, auroc
 
+def gcn_performance(g, features, test_labels, test_ids):
+    in_size = features.shape[1]
+    out_size = 2
+    gcn_model = load_model(Net, path="./model_artifacts/gcn.pth", in_size=in_size, out_size=out_size)
+    gcn_f1, gcn_g_mean, gcn_auroc = performance_metrics(gcn_model, g, features, test_labels, test_ids)
+    return [gcn_f1, gcn_g_mean, gcn_auroc]
+
+def sage_performance(g, features, test_labels, test_ids):
+    in_size = features.shape[1]
+    out_size = 2
+    sage_model = load_model(SAGE, path="./model_artifacts/sage.pth", in_size=in_size, out_size=out_size)
+    sage_f1, sage_g_mean, sage_auroc = performance_metrics(sage_model, g, features, test_labels, test_ids)
+    return [sage_f1, sage_g_mean, sage_auroc]
+
+
+
 
 if __name__ == '__main__':
     g, features, _, _, _, test_ids, _, test_labels = load_elliptic_data(
                 'dataset/ellipticGraph')
-    in_size = features.shape[1]
-    out_size = 2
-    # Load trained models
-    gcn_model = load_model(Net, path="./model_artifacts/gcn.pth", in_size=in_size, out_size=out_size)
-    sage_model = load_model(SAGE, path="./model_artifacts/sage.pth", in_size=in_size, out_size=out_size)
-    ssl_model = load_ssl_model(Encoder, path="./model_artifacts/ssl_model.pth", in_size=166, hid_size=64, out_size=16, decoder_size=2)
-    ssl_classifier = load_log_reg(LogisticRegression, path="./model_artifacts/ssl_classifier.pth", num_dim=16, num_class=2)
+    gcn_metrics = gcn_performance(g, features, test_labels, test_ids)
+    sage_metrics = sage_performance(g, features, test_labels, test_ids)
+    #ssldegree_metrics = ssldegree_performance()
 
-    gcn_f1, gcn_g_mean, gcn_auroc = performance_metrics(gcn_model, g, features, test_labels, test_ids)
-    sage_f1, sage_g_mean, sage_auroc = performance_metrics(sage_model, g, features, test_labels, test_ids)
+    print(gcn_metrics)
+    print(sage_metrics)
+    #print(ssldegree_metrics)
 
-    g, features,_,_, _, _, _, test_ids, _, test_labels = load_elliptic_data_SSL(
-                'dataset/ellipticGraph')
-    ssl_f1, ssl_g_mean, ssl_auroc = performance_metrics_ssl(ssl_classifier, ssl_model, g, features, test_labels, test_ids)
+    # g, features,_,_, _, _, _, test_ids, _, test_labels = load_elliptic_data_SSL(
+    #             'dataset/ellipticGraph')
+    #ssl_f1, ssl_g_mean, ssl_auroc = performance_metrics_ssl(ssl_classifier, ssl_model, g, features, test_labels, test_ids)
 
-    performance_data = pd.DataFrame({
-        "macro_f1":[gcn_f1, sage_f1, ssl_f1],
-        "g_mean": [gcn_g_mean, sage_g_mean, ssl_g_mean], 
-        "AUROC": [gcn_auroc, sage_auroc, ssl_auroc]
-                                     }, index=["GCN", "GraphSAGE", "GraphSSL"])
-    performance_data.to_csv("./performance/results.csv")
+    # performance_data = pd.DataFrame({
+    #     "macro_f1":[gcn_f1, sage_f1, ],
+    #     "g_mean": [gcn_g_mean, sage_g_mean, ], 
+    #     "AUROC": [gcn_auroc, sage_auroc, ]
+    #                                  }, index=["GCN", "GraphSAGE", "GraphSSL"])
+    # performance_data.to_csv("./performance/results.csv")
 
-    print("GCN Metrics: Macro F1-{}, Geometric Mean-{}, AUROC-{}".format(
-        gcn_f1, gcn_g_mean, gcn_auroc))
-    print("GraphSAGE Metrics: Macro F1-{}, Geometric Mean-{}, AUROC-{}".format(
-        sage_f1, sage_g_mean, sage_auroc))
-    print("GraphSSL Metrics: Macro F1-{}, Geometric Mean-{}, AUROC-{}".format(
-        ssl_f1, ssl_g_mean, ssl_auroc))
-    print("Done running performance metrics")
+    # print("GCN Metrics: Macro F1-{}, Geometric Mean-{}, AUROC-{}".format(
+    #     gcn_f1, gcn_g_mean, gcn_auroc))
+    # print("GraphSAGE Metrics: Macro F1-{}, Geometric Mean-{}, AUROC-{}".format(
+    #     sage_f1, sage_g_mean, sage_auroc))
+    # #print("GraphSSL Metrics: Macro F1-{}, Geometric Mean-{}, AUROC-{}".format(
+    # #    ssl_f1, ssl_g_mean, ssl_auroc))
+    # print("Done running performance metrics")
 
 

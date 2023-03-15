ea38ab4fd5a1c228a7ba36f7389f8cfc25013cca
Nigel Doering <nfdoerin@ucsd.edu>
2023-03-14 19:08:01 -0700

    Add performance metrics for dgi classifier

--- performance.py
+++ performance.py
@@ -1,5 +1,7 @@
 #import numpy as np
+from dgi import DGI, Classifier
 import torch as th
+import torch.nn as nn
 from gcn import Net
 from graphSAGE import SAGE
 from graphSSL import LogisticRegression, Encoder, train_ssl_logistic_model
@@ -47,13 +49,28 @@ def performance_metrics_ssldegree(encoder, classifier, g, features, labels, mask
         embeddings = encoder(g, features)
     _embeddings = embeddings.detach()
 
-    logits, _ = classifier(_embeddings[test_ids], test_labels)
+    logits, _ = classifier(_embeddings[mask], labels)
     preds = th.argmax(logits, dim=1)
 
     f1 = metrics.f1_score(test_labels, preds, average="macro")
     auroc = metrics.roc_auc_score(test_labels, preds)
     g_mean = geometric_mean_score(test_labels, preds)
     return f1, g_mean, auroc
+
+def performance_metrics_dgi(dgi, classifier, features, labels, mask):
+    embeds = dgi.encoder(features, corrupt=False)
+    embeds = embeds.detach()
+
+    classifier.eval()
+    with th.no_grad():
+        logits = classifier(embeds)
+        logits = logits[mask]
+        _, preds = th.max(logits, dim=1)
+
+    dgi_f1 = metrics.f1_score(labels, preds, average="macro")
+    dgi_auroc = metrics.roc_auc_score(labels, preds)
+    dgi_g_mean = geometric_mean_score(labels, preds)
+    return dgi_f1, dgi_g_mean, dgi_auroc
 
 def gcn_performance(g, features, test_labels, test_ids):
     in_size = features.shape[1]
@@ -77,6 +94,15 @@ def ssldegree_performance(g, features, train_ids, train_labels, test_labels, tes
      ssldegree_f1, ssldegree_g_mean, ssldegree_auroc = performance_metrics_ssldegree(encoder, classifier, g, features, test_labels, test_ids)
      return [ssldegree_f1, ssldegree_g_mean, ssldegree_auroc]
 
+def dgi_performance(g, features, feature_dim, test_labels, test_ids):
+    dgi = DGI(g, feature_dim, 512, 1, nn.PReLU(512), 0.0)
+    dgi.load_state_dict(th.load("model_artifacts/best_dgi.pkl"))
+    classifier = Classifier(512, 2)
+    classifier.load_state_dict(th.load("model_artifacts/best_dgi_classifier.pth"))
+    dgi_f1, dgi_g_mean, dgi_auroc = performance_metrics_dgi(dgi, classifier, features, test_labels, test_ids)
+    return [dgi_f1, dgi_g_mean, dgi_auroc]
+
+
 def graphSSL_performance(g, features, train_ids, train_labels, test_labels, test_ids):
     ssl_model = load_ssl_model(Encoder, in_size=features.shape[1], 
                            hid_size1=64, hid_size2=32, out_size=16, decoder_size=2)
@@ -87,10 +113,11 @@ def graphSSL_performance(g, features, train_ids, train_labels, test_labels, test
 
 if __name__ == '__main__':
     # Get metrics for gcn and graphSAGE
-    g, features, _, _, _, test_ids, _, test_labels = load_elliptic_data(
+    g, features, _, feature_dim, _, test_ids, _, test_labels = load_elliptic_data(
                 'dataset/ellipticGraph')
     gcn_metrics = gcn_performance(g, features, test_labels, test_ids)
     sage_metrics = sage_performance(g, features, test_labels, test_ids)
+    dgi_metrics = dgi_performance(g, features, feature_dim, test_labels, test_ids)
 
     # Get metrics for ssldegree and graphSAGE
     g, features,pimg0,pimg1, num_nodes, feature_dim, train_ids, test_ids, train_labels, test_labels = load_elliptic_data_SSL(
@@ -103,9 +130,10 @@ if __name__ == '__main__':
     print(sage_metrics)
     print(ssldegree_metrics)
     print(graphSSL_metrics)
+    print(dgi_metrics)
 
-    data = [gcn_metrics, sage_metrics, ssldegree_metrics, graphSSL_metrics]
-    metrics = pd.DataFrame(data, columns=["F1 Score", "Geo Mean", "AUROC"], index=["GCN", "GraphSAGE", "SSLDegree", "GraphSSL"])
+    data = [gcn_metrics, sage_metrics, ssldegree_metrics, graphSSL_metrics, dgi_metrics]
+    metrics = pd.DataFrame(data, columns=["F1 Score", "Geo Mean", "AUROC"], index=["GCN", "GraphSAGE", "SSLDegree", "GraphSSL", "DGI"])
     metrics.to_csv("./performance/results.csv")
 
     # g, features,_,_, _, _, _, test_ids, _, test_labels = load_elliptic_data_SSL(
